8fcff05f546c359d14d316cb5c8dd37177da02a7
Steffen Eckardt <steffen.eckardt@outlook.de>
2022-08-26 18:53:57 +0200

    Added package deploy tp pipeline

--- processor/build.gradle.kts
+++ processor/build.gradle.kts
@@ -5,6 +5,7 @@ val composeVersion: String by project
 plugins {
     kotlin("jvm")
     id("com.google.devtools.ksp")
+    `maven-publish`
 }
 
 ksp {
@@ -34,10 +35,23 @@ dependencies {
     implementation(kotlin("reflect"))
 }
 
-tasks.test {
-    useJUnitPlatform()
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/SteffenEckardt/Compose-Navigation-Generator")
+            credentials {
+                password = project.findProperty("gpr.token") as String? ?: System.getenv("TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["java"])
+        }
+    }
 }
 
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-    kotlinOptions.freeCompilerArgs += "-opt-in=com.squareup.kotlinpoet.ksp.KotlinPoetKspPreview"
+tasks.test {
+    useJUnitPlatform()
 }
